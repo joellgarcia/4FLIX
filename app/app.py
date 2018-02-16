01ccb6003b3e9e74df75ac37c203c63e26f5897a
Odilon Junior <odilon.junior93@gmail.com>
2018-02-16 11:52:19 -0200

    sleep novamente com 0.010

--- app/app.py
+++ app/app.py
@@ -23,7 +23,7 @@ def filme_geral():
     dic = {'filmes': []}
 
     for f in filmes:
-        time.sleep(0.100)
+        time.sleep(0.010)
         f['id'] = str(f['_id'])
         f.pop('_id', None)
         if 'comentarios' in f.keys():
